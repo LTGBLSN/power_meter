bec466dc6487b41a69bdff21d8bc8cd19a9be828
GruBaXu <108203631+LTGBLSN@users.noreply.github.com>
2025-07-14 13:40:35 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/power_meter.elf.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/RoboMaster_git/power_meter/Core/Startup/startup_stm32f103c8tx.s" "D:/RoboMaster_git/power_meter/cmake-build-debug/CMakeFiles/power_meter.elf.dir/Core/Startup/startup_stm32f103c8tx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/RoboMaster_git/power_meter/Core/Inc"
+  "D:/RoboMaster_git/power_meter/Drivers/STM32F1xx_HAL_Driver/Inc"
+  "D:/RoboMaster_git/power_meter/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "D:/RoboMaster_git/power_meter/Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "D:/RoboMaster_git/power_meter/Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/RoboMaster_git/power_meter/Core/Src/gpio.c" "CMakeFiles/power_meter.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Core/Src/gpio.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Core/Src/main.c" "CMakeFiles/power_meter.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Core/Src/main.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Core/Src/stm32f1xx_hal_msp.c" "CMakeFiles/power_meter.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Core/Src/stm32f1xx_it.c" "CMakeFiles/power_meter.elf.dir/Core/Src/stm32f1xx_it.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Core/Src/stm32f1xx_it.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Core/Src/syscalls.c" "CMakeFiles/power_meter.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Core/Src/sysmem.c" "CMakeFiles/power_meter.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Core/Src/sysmem.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Core/Src/system_stm32f1xx.c" "CMakeFiles/power_meter.elf.dir/Core/Src/system_stm32f1xx.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Core/Src/system_stm32f1xx.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj.d"
+  "D:/RoboMaster_git/power_meter/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj.d
@@ -0,0 +1,32 @@
+CMakeFiles/power_meter.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj: \
+ D:\RoboMaster_git\power_meter\Drivers\STM32F1xx_HAL_Driver\Src\stm32f1xx_hal_pwr.c \
+ D:\RoboMaster_git\power_meter\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal.h \
+ D:\RoboMaster_git\power_meter\Core\Inc/stm32f1xx_hal_conf.h \
+ D:\RoboMaster_git\power_meter\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_rcc.h \
+ D:\RoboMaster_git\power_meter\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_def.h \
+ D:\RoboMaster_git\power_meter\Drivers\CMSIS\Device\ST\STM32F1xx\Include/stm32f1xx.h \
+ D:\RoboMaster_git\power_meter\Drivers\CMSIS\Device\ST\STM32F1xx\Include/stm32f103xb.h \
+ D:\RoboMaster_git\power_meter\Drivers\CMSIS\Include/core_cm3.h \
+ C:/CLionToolchains/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/13.3.1/include/stdint.h \
+ C:/CLionToolchains/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdint.h \
+ C:/CLionToolchains/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h \
+ C:/CLionToolchains/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h \
+ C:/CLionToolchains/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h \
+ C:/CLionToolchains/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h \
+ C:/CLionToolchains/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h \
+ D:\RoboMaster_git\power_meter\Drivers\CMSIS\Include/cmsis_version.h \
+ D:\RoboMaster_git\power_meter\Drivers\CMSIS\Include/cmsis_compiler.h \
+ D:\RoboMaster_git\power_meter\Drivers\CMSIS\Include/cmsis_gcc.h \
+ D:\RoboMaster_git\power_meter\Drivers\CMSIS\Device\ST\STM32F1xx\Include/system_stm32f1xx.h \
+ D:\RoboMaster_git\power_meter\Drivers\STM32F1xx_HAL_Driver\Inc/Legacy/stm32_hal_legacy.h \
+ C:/CLionToolchains/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/13.3.1/include/stddef.h \
+ D:\RoboMaster_git\power_meter\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_rcc_ex.h \
+ D:\RoboMaster_git\power_meter\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_gpio.h \
+ D:\RoboMaster_git\power_meter\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_gpio_ex.h \
+ D:\RoboMaster_git\power_meter\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_exti.h \
+ D:\RoboMaster_git\power_meter\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_dma.h \
+ D:\RoboMaster_git\power_meter\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_dma_ex.h \
+ D:\RoboMaster_git\power_meter\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_cortex.h \
+ D:\RoboMaster_git\power_meter\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_flash.h \
+ D:\RoboMaster_git\power_meter\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_flash_ex.h \
+ D:\RoboMaster_git\power_meter\Drivers\STM32F1xx_HAL_Driver\Inc/stm32f1xx_hal_pwr.h
